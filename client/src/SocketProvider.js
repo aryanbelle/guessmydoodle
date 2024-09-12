d9c59f511e9e54e0a5d6c17a8ca4748849590876
aryanbelle <114353667+aryanbelle@users.noreply.github.com>
2024-09-13 03:53:10 +0530

    Update SocketProvider.js

--- client/src/SocketProvider.js
+++ client/src/SocketProvider.js
@@ -7,7 +7,7 @@ export const SocketProvider = ({ children }) => {
     const [socket, setSocket] = useState(null);
 
     useEffect(() => {
-        const socketInstance =  io("https://guessmydoodle.vercel.app");
+        const socketInstance =  io("https://guessmydoodle-server.vercel.app");
  // Adjust URL as needed
         setSocket(socketInstance);
 
